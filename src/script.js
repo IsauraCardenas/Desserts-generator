64d601b9e46c70d2d8a65bab1ef3a51c719211cb
IsauraCardenas <isauracardenas9@gmail.com>
2024-05-23 22:14:08 -0400

    Fixed blink animation

--- src/script.js
+++ src/script.js
@@ -15,7 +15,7 @@ function generatorDesserts(event) {
   let apiKey = "8df22e207cb3offb32384a26df4ddft4";
   let prompt = `Generate one dessert idea using ${instructionInput.value} `;
   let context =
-    "You are a chef, creating delicious easy to-make desserts. Please include a LIST of ingredients and instructions on how to make. Please, make it short.";
+    "You are a chef, creating delicious easy to-make desserts. Please include a LIST of ingredients and instructions on how to make. Please, make it short, and provide a empty line between ingredients and instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let dessertsElement = document.querySelector("#desserts");
